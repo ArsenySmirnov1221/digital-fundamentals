6a7a1c669f5c1ac7035eaea569642d74f6208839
ArsenySmirnov1221 <Ar.Al.Sm@yandex.ru>
2025-11-12 23:44:44 +0300

    Add files via upload

---
+++ scripts/analyser4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+min=1000
+max=-1
+
+for i in $(ls $1)
+do
+	for j in $(cat $1$i)
+	do
+		age=$(echo $j | cut -d ":" -f2)
+		if [ $age -lt $min ]
+		then
+			min=$age
+		elif [ $age -gt $max ]
+		then
+			max=$age
+		fi
+	done
+done
+
+echo $min
+echo $max
