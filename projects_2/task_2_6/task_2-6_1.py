b16ccd15dcd55d1604ed94c223fd6d819974ba57
Arseniy Smirnov <Ar.Al.Sm@yandex.ru>
2026-02-25 21:50:43 +0300

    Занятие 2, задание 2
    Папка project_2 единым коммитом от греха подальше

--- projects_2/task_2_6/task_2-6_1.py
+++ projects_2/task_2_6/task_2-6_1.py
@@ -6,5 +6,5 @@ elif pH > 0 and pH < 7:
     print("Кислая среда")
 elif pH == 7:
     print("Нейтральная среда (физиологический уровень pH)")
-elif pH > 7:
+else pH > 7:
     print("Щелочная среда")
